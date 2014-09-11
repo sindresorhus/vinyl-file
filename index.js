610b80a39670f4bd4f38d905f7f13f2b01114db1
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-09-11 19:01:24 +0200

    Close GH-1: Implement gulp.src options.

--- index.js
+++ index.js
@@ -4,11 +4,38 @@ var fs = require('graceful-fs');
 var stripBom = require('strip-bom');
 var File = require('vinyl');
 
-exports.read = function (pth, cb) {
+exports.read = function (pth, opts, cb) {
+	opts = opts || {};
+
+	if (typeof opts === 'function') {
+		cb = opts;
+		opts = {};
+	}
+
 	fs.stat(pth, function (err, stat) {
 		if (err) {
 			cb(err);
 			return;
+		}
+
+		var cwd = opts.cwd || process.cwd();
+		var resolvedPath = path.resolve(pth);
+		var base = opts.base || cwd;
+
+		var file = new File({
+			cwd: cwd,
+			base: base,
+			path: resolvedPath,
+			stat: stat,
+		});
+
+		if (opts.read === false) {
+			return cb(null, file);
+		}
+
+		if (opts.buffer === false) {
+			file.contents = fs.createReadStream(pth).pipe(stripBom.stream());
+			return cb(null, file);
 		}
 
 		fs.readFile(pth, function (err, buf) {
@@ -16,24 +43,28 @@ exports.read = function (pth, cb) {
 				cb(err);
 				return;
 			}
-
-			cb(null, new File({
-				cwd: process.cwd(),
-				base: process.cwd(),
-				path: path.resolve(pth),
-				stat: stat,
-				contents: stripBom(buf)
-			}));
+			file.contents = stripBom(buf);
+			cb(null, file);
 		});
 	});
 };
 
-exports.readSync = function (pth) {
+exports.readSync = function (pth, opts) {
+	opts = opts || {};
+
+	var contents;
+
+	if (opts.read !== false) {
+		contents = opts.buffer === false ?
+			fs.createReadStream(pth).pipe(stripBom.stream()) :
+			stripBom(fs.readFileSync(pth));
+	}
+
 	return new File({
-		cwd: process.cwd(),
-		base: process.cwd(),
+		cwd: opts.cwd || process.cwd(),
+		base: opts.base || process.cwd(),
 		path: path.resolve(pth),
 		stat: fs.statSync(pth),
-		contents: stripBom(fs.readFileSync(pth))
+		contents: contents
 	});
 };
--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 'use strict';
 var test = require('ava');
 var vinylFile = require('./');
+var isStream = require('isstream');
 
 test('.read()', function (t) {
-	t.plan(6);
+	t.plan(10);
 
 	vinylFile.read('index.js', function (err, file) {
 		t.assert(file.cwd === process.cwd());
@@ -13,16 +14,44 @@ test('.read()', function (t) {
 		t.assert(Buffer.isBuffer(file.contents));
 		t.assert(file.contents.length > 10);
 	});
+
+	vinylFile.read('index.js', {cwd: 'wow'}, function (err, file) {
+		t.assert(file.cwd === 'wow');
+	});
+
+	vinylFile.read('index.js', {base: 'wow'}, function (err, file) {
+		t.assert(file.base === 'wow');
+	});
+
+	vinylFile.read('index.js', {read: false}, function (err, file) {
+		t.assert(file.contents === null);
+	});
+
+	vinylFile.read('index.js', {buffer: false}, function (err, file) {
+		t.assert(isStream(file.contents));
+	});
 });
 
 test('.readSync()', function (t) {
-	t.plan(6);
+	t.plan(10);
 
-	var file = vinylFile.readSync('index.js')
+	var file = vinylFile.readSync('index.js');
 	t.assert(file.cwd === process.cwd());
 	t.assert(file.base === process.cwd());
 	t.assert(file.path === __dirname + '/index.js');
 	t.assert(typeof file.stat === 'object');
 	t.assert(Buffer.isBuffer(file.contents));
 	t.assert(file.contents.length > 10);
+
+	file = vinylFile.readSync('index.js', {cwd: 'wow'});
+	t.assert(file.cwd === 'wow');
+
+	file = vinylFile.readSync('index.js', {base: 'wow'});
+	t.assert(file.base === 'wow');
+
+	file = vinylFile.readSync('index.js', {read: false});
+	t.assert(file.contents === null);
+
+	file = vinylFile.readSync('index.js', {buffer: false});
+	t.assert(isStream(file.contents));
 });
