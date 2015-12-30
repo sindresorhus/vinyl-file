ad4efec6761df8acd2a1f021a9470ead754382c6
Sam Verschueren <sam.verschueren@gmail.com>
2015-12-30 20:29:12 +0100

    promisify the api

--- test.js
+++ test.js
@@ -1,58 +1,46 @@
-'use strict';
-var path = require('path');
-var test = require('ava');
-var vinylFile = require('./');
-var isStream = require('is-stream');
+import test from 'ava';
+import path from 'path';
+import isStream from 'is-stream';
+import fn from './';
 
-test('.read()', function (t) {
-	t.plan(10);
+test('read', async t => {
+	const index = await fn.read('index.js');
+	t.is(index.cwd, process.cwd());
+	t.is(index.base, process.cwd());
+	t.is(index.path, path.join(__dirname, 'index.js'));
+	t.true(Buffer.isBuffer(index.contents));
+	t.true(index.contents.length > 10);
 
-	vinylFile.read('index.js', function (err, file) {
-		t.assert(file.cwd === process.cwd());
-		t.assert(file.base === process.cwd());
-		t.assert(file.path === path.join(__dirname, 'index.js'));
-		t.assert(typeof file.stat === 'object');
-		t.assert(Buffer.isBuffer(file.contents));
-		t.assert(file.contents.length > 10);
-	});
+	const readme = await fn.read('readme.md', {cwd: 'node_modules/ava'});
+	t.true(/Futuristic test runner/.test(readme.contents.toString('utf8')));
 
-	vinylFile.read('readme.md', {cwd: 'node_modules/ava'}, function (err, file) {
-		t.assert(/^# ava/.test(file.contents));
-	});
+	const wow = await fn.read('index.js', {base: 'wow'});
+	t.is(wow.base, 'wow');
 
-	vinylFile.read('index.js', {base: 'wow'}, function (err, file) {
-		t.assert(file.base === 'wow');
-	});
+	const noContents = await fn.read('index.js', {read: false});
+	t.same(noContents.contents, null);
 
-	vinylFile.read('index.js', {read: false}, function (err, file) {
-		t.assert(file.contents === null);
-	});
-
-	vinylFile.read('index.js', {buffer: false}, function (err, file) {
-		t.assert(isStream(file.contents));
-	});
+	const stream = await fn.read('index.js', {buffer: false});
+	t.true(isStream(stream.contents));
 });
 
-test('.readSync()', function (t) {
-	t.plan(10);
+test('read sync', t => {
+	const index = fn.readSync('index.js');
+	t.is(index.cwd, process.cwd());
+	t.is(index.base, process.cwd());
+	t.is(index.path, path.join(__dirname, 'index.js'));
+	t.true(Buffer.isBuffer(index.contents));
+	t.true(index.contents.length > 10);
 
-	var file = vinylFile.readSync('index.js');
-	t.assert(file.cwd === process.cwd());
-	t.assert(file.base === process.cwd());
-	t.assert(file.path === path.join(__dirname, 'index.js'));
-	t.assert(typeof file.stat === 'object');
-	t.assert(Buffer.isBuffer(file.contents));
-	t.assert(file.contents.length > 10);
+	const readme = fn.readSync('readme.md', {cwd: 'node_modules/ava'});
+	t.true(/Futuristic test runner/.test(readme.contents.toString('utf8')));
 
-	file = vinylFile.readSync('readme.md', {cwd: 'node_modules/ava'});
-	t.assert(/^# ava/.test(file.contents));
+	const wow = fn.readSync('index.js', {base: 'wow'});
+	t.is(wow.base, 'wow');
 
-	file = vinylFile.readSync('index.js', {base: 'wow'});
-	t.assert(file.base === 'wow');
+	const noContents = fn.readSync('index.js', {read: false});
+	t.same(noContents.contents, null);
 
-	file = vinylFile.readSync('index.js', {read: false});
-	t.assert(file.contents === null);
-
-	file = vinylFile.readSync('index.js', {buffer: false});
-	t.assert(isStream(file.contents));
+	const stream = fn.readSync('index.js', {buffer: false});
+	t.true(isStream(stream.contents));
 });
